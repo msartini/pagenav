cad142852eba14a9a5b41eb701c22052411aebb8
marciosartini <marcio.sartini@dclick.com.br>
2014-04-23 10:41:25 -0300

    melhorias na navegacao

--- js/app.js
+++ js/app.js
@@ -1,16 +1,28 @@
 $(document).ready(function(){
 
 	var page = "#page1";
+	var direction = "";
 
 	 
 	$('a.callSobre').click( 'click', function() {
 		page = $(this).data("origem"); 
 	});
 
+	$('#btnPage1Up').click ('click', function(){
+	 	direction = $(this).data("directioninverse"); 
+		page = $(this).data("origem"); 
+	});
 
-	$('#btnSobreVoltar').click( 'click', function() {
-		
-		$(this).attr('href', page); 
+
+	$('.btnVoltar').click( 'click', function() {
+	 
+		var pagina = page;
+		var directionpagina = direction;
+		direction = "";
+		page = "";
+		 
+		$(this).data('transition', directionpagina); 
+		$(this).attr('href', pagina); 
 	   
 	});
 
