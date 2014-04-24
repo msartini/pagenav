461f67da892dff4ad997dc91c10683e0ff9f0981
marciosartini <marcio.sartini@dclick.com.br>
2014-04-24 15:04:31 -0300

    navegacao

--- js/app.js
+++ js/app.js
@@ -1,15 +1,17 @@
 $(document).ready(function(){
 
 	var page = "#page1";
+	var transaction = "";
 	var direction = "";
 
 	 
 	$('a.callSobre').click( 'click', function() {
+		direction = '';
 		page = $(this).data("origem"); 
 	});
 
 	$('#btnPage1Up').click ('click', function(){
-	 	direction = $(this).data("directioninverse"); 
+	 	transaction = $(this).data("directioninverse"); 
 		page = $(this).data("origem"); 
 	});
 
@@ -17,14 +19,86 @@ $(document).ready(function(){
 	$('.btnVoltar').click( 'click', function() {
 	 
 		var pagina = page;
-		var directionpagina = direction;
-		direction = "";
+		var transactionpagina = transaction;
+		transaction = "";
 		page = "";
+		if (direction != "") {
+			$(this).data('direction', direction); 
+		}
 		 
-		$(this).data('transition', directionpagina); 
+		$(this).data('transition', transactionpagina); 
 		$(this).attr('href', pagina); 
 	   
 	});
+
+	$('.mapVoltar').click( 'click', function() {
+	 
+		var pagina = page;
+		var transactionpagina = transaction;
+		transaction = "";
+		page = "";
+		 
+		$(this).data('transition', transactionpagina); 
+		$(this).attr('href', pagina); 
+	   
+	});
+
+
+
+
+ 
+
+	var timeoutId = null;
+ 
+
+	var valorDelta;
+	var pagedown = "";
+	var pageup = "";
+	 
+
+	var mousewheelevt = (/Firefox/i.test(navigator.userAgent)) ? "DOMMouseScroll" : "mousewheel" //FF doesn't recognize mousewheel as of FF3.x
+	
+	$('.scrollSobre').bind(mousewheelevt, function(e){
+
+			pagedown = $(this).data('pagedown');
+			page = $(this).data("origem"); 
+			pageup = $(this).data('pageup');
+
+
+		    var evt = window.event || e //equalize event object     
+		    evt = evt.originalEvent ? evt.originalEvent : evt; //convert to originalEvent if possible               
+		    valorDelta = evt.detail ? evt.detail*(-40) : evt.wheelDelta //check for detail first, because it is used by Opera and FF
+		   // console.log($(document).scrollTop().valueOf());
+		    clearTimeout(timeoutId);
+			if(   ( $(window).scrollTop() + $(window).height() == $(document).height() ) || $(document).scrollTop().valueOf() <= 0   ) {
+			    timeoutId = setTimeout(callPage, 30);
+	    	}
+	    
+	});
+
+	function callPage() {
+		if(valorDelta > 0) {
+	        
+			//console.log ('scrolling down' + + timeoutId + ' -  ' + pagedown);
+	        if (pageup != "") {
+	        	transaction = "slidedown";
+	        	direction = "";
+	        	$.mobile.changePage( pageup, { transition: transaction} );	
+	        }
+
+
+	    }
+	    else{
+	        //console.log ('scrolling down' + + timeoutId + ' -  ' + pagedown);
+	        if (pagedown != "") {
+	        	transaction = "slideup";
+	        	direction = "";
+	        	$.mobile.changePage( pagedown, { transition: transaction} );	
+	        }
+
+
+	    }   
+	}
 
 	
 
